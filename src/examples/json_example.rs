54eaf9a42da1f5190f8fb538f9caf672efd12210
Shacham <shacham6@gmail.com>
2017-11-04 13:57:16 +0200

    Did a few things and now everythong is cool

---
+++ src/examples/json_example.rs
@@ -0,0 +1,47 @@
+use serde;
+use serde_json;
+use serde_derive;
+
+use std::fs::File;
+use std::io::Read;
+use std::vec::Vec;
+use std::env::args;
+use std::io::Error;
+
+use serde::de::{Deserialize, DeserializeOwned};
+use serde_yaml::from_str as yaml_from_str;
+
+
+pub fn run(filepath: String) {
+    let mut string = String::new();
+    read_content_of_file_to_string(filepath.as_str(), &mut string);
+    let thingy = serde_json::from_str::<Thingy>(string.as_str()).expect("Couldn't parse the file");
+
+    println!("\n\nhead = {}", thingy.head);
+
+    thingy.requests.iter().for_each(|request| {
+        println!("\tname = {}", request.name);
+        if let Some(age) = request.age {
+            println!("\t{} has the age property. age = {}", request.name, age);
+        }
+        println!();
+    });
+}
+
+pub fn read_content_of_file_to_string<'a>(filepath: &'a str, buffer: &mut String) -> Result<usize, Error> {
+    let mut file = File::open(filepath).unwrap();
+    file.read_to_string(buffer)
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Thingy {
+    head: String,
+    requests: Vec<PrivateRequest>
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct PrivateRequest {
+    name: String,
+    age: Option<u32>,
+    active: bool
+}
---
+++ src/examples/yaml_example.rs
@@ -0,0 +1,8 @@
+use serde;
+use serde_derive;
+use serde_yaml;
+
+use std::env::args;
+use std::vec::Vec;
+
+pub fn run(filepath: String) {}
